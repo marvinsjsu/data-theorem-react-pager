d6a99200035c6c30c9f5c2a2ea39ebb1588d6fc4
Marvin Mante <marvinsjsu@gmail.com>
2019-08-07 12:10:24 -0700

    update import to api

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ var _react = _interopRequireDefault(require("react"));
 
 var _propTypes = _interopRequireDefault(require("prop-types"));
 
-var _api = require("../utils/api");
+var _api = require("./utils/api");
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 
